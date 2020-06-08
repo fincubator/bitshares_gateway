b030b0eac224a2c4adc6af7e4e9dd2a10cca819a
biobdeveloper <ibogomolov321@gmail.com>
2020-06-09 02:12:44 +0300

    Config refactoring
    Signed-off-by: biobdeveloper <ibogomolov321@gmail.com>

--- config/__init__.py
+++ config/__init__.py
@@ -7,3 +7,6 @@ project_root_dir = Path(__file__).parent.parent
 
 cfg = yaml.safe_load(open(f"{project_root_dir}/config/config.yml", 'r'))
 
+zmq_cfg = cfg["zqm_server"]
+
+gateway_cfg = cfg["gateway"]
