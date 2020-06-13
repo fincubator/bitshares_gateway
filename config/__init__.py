62057e0378b391e6a2864739ec818b03e6c4336e
biobdeveloper <ibogomolov321@gmail.com>
2020-06-13 03:21:45 +0300

    Replase deprecated parts in conn_url
    Signed-off-by: biobdeveloper <ibogomolov321@gmail.com>

--- config/__init__.py
+++ config/__init__.py
@@ -12,6 +12,6 @@ zmq_cfg = cfg["zqm_server"]
 gateway_cfg = cfg["gateway"]
 
 pg_config = cfg["postgres"]
-sql_conn_url = f"postgres+psycopg2://{pg_config['user']}:{pg_config['password']}" \
+sql_conn_url = f"postgresql+psycopg2://{pg_config['user']}:{pg_config['password']}" \
                f"@" \
                f"{pg_config['host']}:{pg_config['port']}/{pg_config['database']}"
