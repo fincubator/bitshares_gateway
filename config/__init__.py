424297fa0fac1a20b02dfdab83594c975419065e
biobdeveloper <ibogomolov321@gmail.com>
2020-06-19 18:38:31 +0300

    Refactoring configs
    Signed-off-by: biobdeveloper <ibogomolov321@gmail.com>

--- config/__init__.py
+++ config/__init__.py
@@ -1,17 +1,45 @@
-import yaml
 from pathlib import Path
+import os
+
+import yaml
+from dotenv import load_dotenv
 
 from config.const import *
 
+
+load_dotenv()
+
 project_root_dir = Path(__file__).parent.parent
 
-cfg = yaml.safe_load(open(f"{project_root_dir}/config/config.yml", 'r'))
+try:
+    gateway_cfg = yaml.safe_load(open(f"{project_root_dir}/config/gateway.yml", 'r'))
 
-zmq_cfg = cfg["zqm_server"]
+except FileNotFoundError:
+    print("Use testnet config from fixtures!")
+    # If config/gateway.yml is not provided, using testnet fixtures"
+    from tests.fixtures import \
+        testnet_gateway_account, testnet_gateway_memo, testnet_gateway_active, \
+        testnet_gateway_prefix, testnet_core_asset, testnet_bitshares_nodes
 
-gateway_cfg = cfg["gateway"]
+    gateway_cfg = {
+        'core_asset': testnet_core_asset,
+        'gateway_core_asset': testnet_core_asset,
+        'gateway_distribute_asset': 'ETH',
+        'account': testnet_gateway_account,
+        'keys': {'active': testnet_gateway_active,
+                 'memo': testnet_gateway_memo},
+        'nodes': testnet_bitshares_nodes
+    }
 
-pg_config = cfg["postgres"]
+
+pg_config = {
+    'host': os.getenv("DATABASE_HOST"),
+    'port': os.getenv("DATABASE_PORT"),
+    'user': os.getenv("DATABASE_USERNAME"),
+    'password': os.getenv("DATABASE_PASSWORD"),
+    'database': os.getenv("DATABASE_NAME")
+}
+
 sql_conn_url = f"postgresql+psycopg2://{pg_config['user']}:{pg_config['password']}" \
                f"@" \
                f"{pg_config['host']}:{pg_config['port']}/{pg_config['database']}"
