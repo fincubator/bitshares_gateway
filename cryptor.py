5e89fef63b7977af8a7283c0c805d412215c2068
biobdeveloper <ibogomolov321@gmail.com>
2020-06-25 00:30:05 +0300

    Implement cryptor module
    Signed-off-by: biobdeveloper <ibogomolov321@gmail.com>

--- cryptor.py
+++ cryptor.py
@@ -7,5 +7,5 @@ def encrypt(string_to_encrypt: str, password: str) -> str:
     return b64encode(rncryptor.RNCryptor().encrypt(data=string_to_encrypt, password=password)).decode('utf-8')
 
 
-def decrypt(ecnrypted_string: str, password: str) -> str:
-    return rncryptor.RNCryptor().decrypt(b64decode(ecnrypted_string.encode('utf-8')), password=password)
+def decrypt(encrypted_string: str, password: str) -> str:
+    return rncryptor.RNCryptor().decrypt(b64decode(encrypted_string.encode('utf-8')), password=password)
