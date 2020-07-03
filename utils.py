fe56b0ebbe0c3a2ba0157b99d411f047c11b12cf
biobdeveloper <ibogomolov321@gmail.com>
2020-07-03 13:26:31 +0300

    Custom logger implement
    Signed-off-by: biobdeveloper <ibogomolov321@gmail.com>

--- utils.py
+++ utils.py
@@ -1,4 +1,8 @@
+"""Small stand-alone utils in one place"""
+import logging
 import aiohttp
+from aiopg.sa.result import RowProxy
+from sqlalchemy import inspect
 
 from config import CONTROLCENTERURL
 
@@ -14,3 +18,27 @@ async def get_gw_settings(gw, url=CONTROLCENTERURL):
                         return gw_settings
             except Exception as ex:
                 raise Exception(f"Unable to fetch gateway {gw} settings: {ex}")
+
+
+def get_logger(name: str) -> logging.Logger:
+    log = logging.getLogger(name)
+    log.setLevel(level=logging.INFO)
+    formatter = logging.Formatter("%(asctime)s|%(name)s|%(levelname)s|%(message)s")
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+    return log
+
+
+def object_as_dict(obj):
+    """Represent any sqlalchemy model object as python dict"""
+    return {c.key: getattr(obj, c.key) for c in inspect(obj).mapper.column_attrs}
+
+
+def rowproxy_to_dto(row_proxy: RowProxy, from_, to_):
+    """Convert sqlalchemy result to Marshmallow DataTransferObject"""
+    model = from_(**row_proxy)
+    model_dict = object_as_dict(model)
+    model_dict.pop("pk")
+    instance = to_(**model_dict)
+    return instance
